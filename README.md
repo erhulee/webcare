f855b14bcbc01a491719432a535ce4df70afb8aa
erhu <1574605559@qq.com>
2023-04-09 01:41:06 +0800

    tag: npm发包

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # sniper-sdk
+
 【Doing】 监控 sdk
 ![sdk-mindmap](https://user-images.githubusercontent.com/57227420/230600705-41d0a17b-eaa5-4264-b36b-397f7da06b9d.png)
--- packages/web/plugins/behavior/events.ts
+++ packages/web/plugins/behavior/events.ts
@@ -1,21 +1,18 @@
 
-// import {} from "web-vitals"
-import  WebMonitor  from "web/WebMonitor"
-import {Plugin} from "share/Plugin"
-// import { createBrowserHistory } from "history"
-// import { captureNativeFn } from "web/utils/captureNativeFn.ts"
+import WebMonitor from "web/WebMonitor"
+import { Plugin } from "share/Plugin"
 
 /*
     history: popState 事件
     hash: pushSate / replaceState 重写
 */
 
-export class EventsPlugin implements Plugin{
-    instance:WebMonitor
+export class EventsPlugin implements Plugin {
+    instance: WebMonitor
     eventKeys = ["click", "input", "keydown", "keyup"]
-    collectEventHandle = (e:any)=>{
+    collectEventHandle = (e: any) => {
         const pathName = location.pathname;
-        while(this.instance.eventStack.length >= 20){
+        while (this.instance.eventStack.length >= 20) {
             this.instance.eventStack.shift();
         }
         this.instance.eventStack.push({
@@ -23,19 +20,19 @@ export class EventsPlugin implements Plugin{
             event: e
         })
     }
-    constructor(instance:WebMonitor){
+    constructor(instance: WebMonitor) {
         this.instance = instance
     }
-    init(){
+    init() {
 
     }
-    async run(){
-        this.eventKeys.forEach((event)=>{
+    async run() {
+        this.eventKeys.forEach((event) => {
             document.addEventListener(event, this.collectEventHandle)
         })
     }
-    unload(){
-        this.eventKeys.forEach((event)=>{
+    unload() {
+        this.eventKeys.forEach((event) => {
             document.removeEventListener(event, this.collectEventHandle)
         })
     }
