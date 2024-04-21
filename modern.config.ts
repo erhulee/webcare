b5c69743cd63eab5e12e17962d38cadeb7d86640
erhu <1574605559@qq.com>
2024-04-21 14:28:11 +0800

    docs: 文档开始搭建

--- modern.config.ts
+++ modern.config.ts
@@ -1,7 +1,17 @@
 import { moduleTools, defineConfig } from "@modern-js/module-tools";
-
+import { modulePluginDoc } from "@modern-js/plugin-rspress";
 export default defineConfig({
-  plugins: [moduleTools()],
+  plugins: [
+    moduleTools(),
+    modulePluginDoc({
+      doc: {
+        root: "docs",
+        markdown: {
+          defaultWrapCode: true
+        }
+      },
+    }),
+  ],
   // 指定构建预设配置
   buildPreset: "npm-library",
   buildConfig: {
